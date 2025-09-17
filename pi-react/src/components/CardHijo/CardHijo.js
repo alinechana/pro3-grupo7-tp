25a1a812993a84c020473b1eb278925d709f0310
JustinaNavarro <justinanavarro@udesa.edu.ar>
2025-09-17 13:43:51 -0300

    css

--- pi-react/src/components/CardHijo/CardHijo.js
+++ pi-react/src/components/CardHijo/CardHijo.js
@@ -30,33 +30,41 @@ class CardHijo extends Component {
     render() {
         return (
             <section className="card-container">
-                <img
-                    className="card-img"
-                    src={`https://image.tmdb.org/t/p/w500${this.props.pelicula.poster_path}`}
-                    alt={this.props.pelicula.title}
-                />
+                <article class="single-card-movie">
+                    <img
+                        className="card-img"
+                        src={`https://image.tmdb.org/t/p/w500${this.props.pelicula.poster_path}`}
+                        alt={this.props.pelicula.title}
+                    />
+                    <div class="cardBody">
+                        
+                            <Link to={`/detalle/${this.props.pelicula.id}`} >
+                                <h3 className="card-title">{this.props.pelicula.title}</h3>
 
-           <div className="title"> 
-           <Link to={`/detalle/${this.props.pelicula.id}`} > 
-                <h3 className="card-title">{this.props.pelicula.title}</h3>
-        
-                </Link>
-           </div>
-               
+                            </Link>
+                      
 
-                <div className="div-description"> 
 
-                 {this.state.verDescripcion && ( //si verdescripcion es true, muestra la descripcion
-                    <p className="card-description">{this.props.pelicula.overview}</p>
-                )}
+                        <div className="div-description">
 
-                <button className="b-description" onClick={this.cambiar}>{this.state.textoBoton}</button> </div>
+                            {this.state.verDescripcion && ( //si verdescripcion es true, muestra la descripcion
+                                <p className="card-description">{this.props.pelicula.overview}</p>
+                            )}
 
-                
+                            <button className="b-description" onClick={this.cambiar}>{this.state.textoBoton}</button> </div>
 
-                
 
-                
+
+                    </div>
+
+
+
+
+                </article>
+
+
+
+
             </section>
 
 
